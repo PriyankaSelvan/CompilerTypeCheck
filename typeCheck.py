9a1102a3daded01cc9e8c15424b29aff1d716c67
Priyanka <priyanka.nessie@gmail.com>
2017-08-31 08:42:08 +0530

    corrected

--- typeCheck.py
+++ typeCheck.py
@@ -4,6 +4,7 @@ import enum
 
 global variable_name
 
+#Tags for types
 class pytypes(enum.Enum):
       int_ = 0
       bool_ = 1
@@ -11,54 +12,48 @@ class pytypes(enum.Enum):
 
 def type_check(node):
     global variable_name
-    print "-----node is-----"
-    print node
-    children = node.getChildNodes()
-    for child in children:
-        print "------child is-------"
-        print child
-        if str(child).startswith("AssName"):
-           print "Its an assign node"
-           variable_name = child.name
-        elif str(child).startswith("Add"):
-           print "Its a add node"
-           print "caling typechecks "
-           typeleft = type_check(child.getChildNodes()[0])
-           typeright = type_check(child.getChildNodes()[1])
-           print typeleft, typeright
+
+    #Checking each type of node and performing actions
+
+    if str(node).startswith("Assign"):
+           variable_name =  node.getChildNodes()[0].name
+           return type_check(node.getChildNodes()[1])
+
+    elif str(node).startswith("Add"):
+           typeleft = type_check(node.getChildNodes()[0])
+           typeright = type_check(node.getChildNodes()[1])
            if typeleft != pytypes.error_ and typeright != pytypes.error_:
                 return pytypes.int_
-        elif str(child).startswith("And"):
-           print "its an and node"
-           typeleft = type_check(child.getChildNodes()[0])
-           typeright = type_check(child.getChildNodes()[1])
+           else: return pytypes.error_
+
+    elif str(node).startswith("And"):
+           typeleft = type_check(node.getChildNodes()[0])
+           typeright = type_check(node.getChildNodes()[1])
            if typeleft == pytypes.error_ or typeright == pytypes.error_:
               return pytypes.error_
            elif typeleft == pytypes.int_ and typeright == pytypes.int_:
               return pytypes.int_
+           elif typeleft == pytypes.bool_ and typeright == pytypes.int_:
+              return pytypes.int_
            else:
               return pytypes.bool_
            
-        elif str(child).startswith("Not"):
-           print "its a not node"
-           typedown = type_check(child.getChildNodes()[0])
+    elif str(node).startswith("Not"):
+           typedown = type_check(node.getChildNodes()[0])
            if typedown == pytypes.error_:
               return pytypes.error_
            else: return pytypes.bool_
     
-        elif str(child).startswith("Const"):
-           return type_check(child)
-           
-    if str(node).startswith("Const"):
-        print "found the number"
-        print node.value
-        print type(node.value)
+    elif str(node).startswith("Const"):
         if isinstance(node.value,int):
           return pytypes.int_
-        elif isinstance(node.value,bool):
-          return pytypes.bool_
         else: return pytypes.error_
-      
+ 
+    elif str(node).startswith("Name"):
+        if node.name == "True" or node.name == "False":
+           return pytypes.bool_
+        else:
+           return pytypes.error_
            
 
 tree = compiler.parseFile("code.py")
